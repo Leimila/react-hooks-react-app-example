4c900df76b79679fdc816a5ac72e40dee799e24f
Leimila <leimilamitei2016@gmail.com>
2024-11-27 19:54:54 +0300

    Implemented new features and passed tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Article from './Article'
 import Header from './Header'
+import TestComponent from './TestComponent'; // Import TestComponent
+
 
 function App() {
   return <div className="App">
+    <h1>Now</h1> {/* Added <h1> with content 'Now' */}
     learn react!
     <Header/>
     <Article />
+    <TestComponent /> {/* Add the new component here */}
   </div>;
 }
 
 export default App;
+
---
+++ src/components/TestComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TestComponent() {
+  return (
+    <div>
+      <p>This is a test component!</p>
+    </div>
+  );
+}
+
+export default TestComponent;
